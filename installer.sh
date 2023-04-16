ffb38ea2053def659b0678a491381ae3ac7ae78f
Ismael P. Santana <ismakenps@gmail.com>
2023-04-16 14:39:56 -0600

    p e r f e c t ?

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
 chmod +x ./suspender.sh
-cp ./suspender.sh /usr/share/suspender
+cp ./suspender.sh /usr/bin/suspender
+cp ./suspender.service /etc/systemd/system/suspender.service
 systemctl daemon-reload
 systemctl enable suspender.service
 systemctl start suspender.service
